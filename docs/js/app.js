ea66b2524aba64f33b6382d780bd73dfbc674318
Vaibhavarolkar <vaibhav.a@ajency.in>
2021-03-11 12:03:38 +0530

    build

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,30 +1,51 @@
 $(document).ready(function () {
-    
+  rangeSlider();
 });
+
+$(window).resize(function() { 
+  console.log("testtetst")
+  $( "#slider-range-max" ).slider( "destroy" );
+  rangeSlider();
+});
+
+function rangeSlider() {
+  const isMobile = () => window.innerWidth < 768;
+  $("#slider-range-max").slider({
+    range: isMobile() ? "min" : "max",
+    min: 1,
+    max: 4,
+    value: 3,
+    orientation: isMobile() ? "vertical" : "horizontal",
+    slide: function (event, ui) {
+      $("#whisky_knowledge").val(ui.value);
+    }
+  });
+  $("#whisky_knowledge").val($("#slider-range-max").slider("value"));
+}
 
 function onSubmit(token) {
   document.getElementById("demo-form").submit();
 }
 
-var sheet = document.createElement('style'),  
+var sheet = document.createElement('style'),
   $rangeInput = $('.range input'),
   prefs = ['webkit-slider-runnable-track', 'moz-range-track', 'ms-track'];
 
 document.body.appendChild(sheet);
 
-var getTrackStyle = function (el) {  
+var getTrackStyle = function (el) {
   var curVal = el.value,
-      val = (curVal - 1) * 16.666666667,
-      style = '';
-  
+    val = (curVal - 1) * 16.666666667,
+    style = '';
+
   // Set active label
   $('.range-labels li').removeClass('active selected');
-  
+
   var curLabel = $('.range-labels').find('li:nth-child(' + curVal + ')');
-  
+
   curLabel.addClass('active selected');
   curLabel.prevAll().addClass('selected');
-  
+
   // Change background gradient
   for (var i = 0; i < prefs.length; i++) {
     style += '.range {background: linear-gradient(to right, red, red' + val + '%, #fff ' + val + '%, #fff 100%)}';
@@ -41,12 +62,12 @@ $rangeInput.on('input', function () {
 // Change input value on label click
 $('.range-labels li').on('click', function () {
   var index = $(this).index();
-  
+
   $rangeInput.val(index + 1).trigger('input');
-  
+
 });
 
-$( function() {
-  $( "#sortable" ).sortable();
-  $( "#sortable" ).disableSelection();
-} );
+$(function () {
+  $("#sortable").sortable();
+  $("#sortable").disableSelection();
+});
